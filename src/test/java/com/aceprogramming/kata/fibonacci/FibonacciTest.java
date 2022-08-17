03a43276e2e3736020a9ceea3028ddfbdcaf502f
DaveCarlson <davicar319@acm.org>
2022-08-17 14:47:59 -0500

    Clean up unit tests

--- src/test/java/com/aceprogramming/kata/fibonacci/FibonacciTest.java
+++ src/test/java/com/aceprogramming/kata/fibonacci/FibonacciTest.java
@@ -1,45 +1,19 @@
 package com.aceprogramming.kata.fibonacci;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class FibonacciTest {
-
-    @Test
-    void theFibonacciOfZeroIsOne() {
-        testFibonacciOf(1, 0);
-    }
-
-    @Test
-    void theFibonacciOfOneIsOne() {
-        testFibonacciOf(1, 1);
-    }
-
-    @Test
-    void theFibonacciOfTwoIsTwo() {
-        testFibonacciOf(2, 2);
-    }
-
-    @Test
-    void theFibonacciOfThreeIsThree() {
-        testFibonacciOf(3, 3);
-    }
-
-    @Test
-    void theFibonacciOfFourIsFive() {
-        testFibonacciOf(5, 4);
-    }
-
-    @Test
-    void theFibonacciOfFiveIsEight() {
-        testFibonacciOf(8, 5);
-    }
-
-    @Test
-    void theFibonacciOfSixIsThirteen() {
-        testFibonacciOf(8, 5);
-    }
-
+    @ParameterizedTest(name = "Fibonacci of {1} is {0}")
+    @CsvSource({
+            "1,0",
+            "1,1",
+            "2,2",
+            "3,3",
+            "5,4",
+            "8,5",
+            "13,6"})
     void testFibonacciOf(int expected, int input) {
         Assertions.assertEquals(expected, Fibonacci.of(input));
     }
