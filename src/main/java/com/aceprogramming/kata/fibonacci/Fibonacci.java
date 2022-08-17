15ea58b56c5d69b034c695d342d4ab410f244270
DaveCarlson <davicar319@acm.org>
2022-08-17 15:44:34 -0500

    Adding in memoization.

--- src/main/java/com/aceprogramming/kata/fibonacci/Fibonacci.java
+++ src/main/java/com/aceprogramming/kata/fibonacci/Fibonacci.java
@@ -1,7 +1,27 @@
 package com.aceprogramming.kata.fibonacci;
 
+import java.math.BigInteger;
+import java.util.WeakHashMap;
+
 public class Fibonacci {
-    public static int of(int i) {
-        return i < 2 ? 1 : of(i-2) + of(i-1);
+    public static BigInteger of(BigInteger i) {
+        return memoizedFibonacciOf(i);
+    }
+
+    private static WeakHashMap<BigInteger, BigInteger> fibMemo = new WeakHashMap<>();
+    private static BigInteger memoizedFibonacciOf(BigInteger i) {
+        if(fibMemo.containsKey(i)) {
+            return fibMemo.get(i);
+        } else {
+            return calculateFibonacci(i);
+        }
+    }
+
+    private static BigInteger calculateFibonacci(BigInteger i) {
+        var fib =  i.compareTo(BigInteger.TWO) < 0 ?
+                BigInteger.ONE :
+                of(i.subtract(BigInteger.TWO)).add(of(i.subtract(BigInteger.ONE)));
+        fibMemo.put(i, fib);
+        return fib;
     }
 }
--- src/test/java/com/aceprogramming/kata/fibonacci/FibonacciTest.java
+++ src/test/java/com/aceprogramming/kata/fibonacci/FibonacciTest.java
@@ -1,8 +1,12 @@
 package com.aceprogramming.kata.fibonacci;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.math.BigInteger;
+import java.time.Duration;
 
 public class FibonacciTest {
     @ParameterizedTest(name = "Fibonacci of {1} is {0}")
@@ -14,7 +18,13 @@ public class FibonacciTest {
             "5,4",
             "8,5",
             "13,6"})
-    void testFibonacciOf(int expected, int input) {
+    void testFibonacciOf(BigInteger expected, BigInteger input) {
         Assertions.assertEquals(expected, Fibonacci.of(input));
+    }
+
+    @Test
+    void timedTest() {
+        Assertions.assertTimeout(Duration.ofMillis(25),
+                () -> System.out.println(Fibonacci.of(BigInteger.valueOf(2000))));
     }
 }
